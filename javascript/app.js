ec706ba6e00149ea3bd6b616c141660da1f37f09
Moon Child <kaitline.moon@gmail.com>
2018-10-04 16:36:34 -0400

    Added scrolling function, added links to projects, and added jQuery.

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,14 @@
 $(document).ready(() => {
 
+// On click function for scrolling to different sections on the page
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
+
+
+
 });
