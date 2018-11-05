5c5dfa7138c677828a3b9422f8b0ddb215a354d0
Moon Child <kaitline.moon@gmail.com>
2018-11-04 19:02:32 -0500

    Updating scrolling and highlighting on sections.

--- javascript/app.js
+++ javascript/app.js
@@ -1,14 +1,17 @@
 $(document).ready(() => {
 
 // On click function for scrolling to different sections on the page
-$("nav").find("a").click(function(e) {
-    e.preventDefault();
-    var section = $(this).attr("href");
+$("nav").find("a").click(function (event) {
+    event.preventDefault()
+    var section = $(this).attr("href")
     $("html, body").animate({
         scrollTop: $(section).offset().top
-    });
-});
+    })
+})
+
+// Highlights the menu item that corresponds to the section when the section is being viewed
+$('body').scrollspy({ target: '#navigation' })
 
 
 
-});
+})
