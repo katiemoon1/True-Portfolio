a93c2a2d245fdc8ec4ff6f5ffcbf4b2605cec9fa
Katie Moon <kaitline.moon@gmail.com>
2019-01-13 11:58:02 -0500

    Fixed scrolling issues!

--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,7 @@
 $(document).ready(() => {
 
-// On click function for scrolling to different sections on the page
-$("nav").find("a").click( event => {
-    event.preventDefault()
-    var section = $(this).attr("href")
-    $("html, body").animate({
-        scrollTop: $(section).offset().top
-    })
-})
-
 // Highlights the menu item that corresponds to the section when the section is being viewed
 $('body').scrollspy({ target: '#navigation' })
-
-// Starts the carousel of images for my about me section
-$(".carousel").carousel({ interval: 7000 })
 
 // Loading spinner
 $(window).on('load', function() {
