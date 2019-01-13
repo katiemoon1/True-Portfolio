a8cfff468cf7069b8c8483bda203d4be59d50a3f
Katie Moon <kaitline.moon@gmail.com>
2019-01-13 18:17:19 -0500

    Removed irrelevant code.

--- javascript/app.js
+++ javascript/app.js
@@ -8,11 +8,4 @@ AOS.init({
 // Highlights the menu item that corresponds to the section when the section is being viewed
 $('body').scrollspy({ target: '#navigation' })
 
-// Loading spinner
-$(window).on('load', function() {
-    $('#loading').fadeOut('slow', function() {
-        $(this).remove()
-    })
-})
-
 })
