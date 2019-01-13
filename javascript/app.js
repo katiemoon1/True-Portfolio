6c21572a8b9d4f13e76687117bb08d68e2402a4a
Katie Moon <kaitline.moon@gmail.com>
2019-01-13 18:16:01 -0500

    Updated animation, links, photos, and scrolling.

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,10 @@
 $(document).ready(() => {
 
+// Initializing AOS library
+AOS.init({
+    duration: 1200
+})
+
 // Highlights the menu item that corresponds to the section when the section is being viewed
 $('body').scrollspy({ target: '#navigation' })
 
